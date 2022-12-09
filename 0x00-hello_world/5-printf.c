b4452dda775c9abc85a76c19e7f3b941092b68f7
Vichazz <oyeniyivt@gmail.com>
2022-12-08 17:49:45 -0800

    Betty Stubborn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- *main = entry point
- *return: Always 0 (Success)
- */
 
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
