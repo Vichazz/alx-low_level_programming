aaba337517cbd836f3ce208bb1650cc05cfe3e43
Vichazz <oyeniyivt@gmail.com>
2022-12-09 18:50:05 -0800

    ....

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * Return: returns 0
+ */
+
+int main(void)
+{
+int digits;
+for (digits = 0; digits < 10; digits++)
+putchar(digits);
+printf("\n");
+return (0);
+
+}
