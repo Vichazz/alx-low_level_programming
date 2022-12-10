cea58c878c0dcb64dc891b4ab0244a7753d32184
Vichazz <oyeniyivt@gmail.com>
2022-12-09 19:16:11 -0800

    ...

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ * Return: returns 0
+ */
+
+int main(void)
+{
+int alphabet;
+for (alphabet = 'z'; alphabet > 'a'; alphabet--)
+putchar(alphabet);
+putchar('\n');
+return (0);
+
+}
