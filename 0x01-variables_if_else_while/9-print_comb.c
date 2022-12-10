9ffd81d76e65fa82cdc7aabfd3542b21dd6224ac
Vichazz <oyeniyivt@gmail.com>
2022-12-10 05:29:46 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * Return: returns 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10)
+{
+putchar(n);
+n++;
+if (n != 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n')
+
+
+}
