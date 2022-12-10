c223b567252b57f15ae236988a6e50becc2283d4
Vichazz <oyeniyivt@gmail.com>
2022-12-09 17:15:20 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+0-positive_or_negative.c#include <stdio.h>
 
 /**
  * main - program starts
  * Return: returns 0
  */
-
+g
 int main(void)
 {
 char alphabet;
-char ALPHABET;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet)
-for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
-putchar(ALPHABET);
+for (alphabet = 'A'; alphabet  <= 'Z'; alphabet++)
+putchar(alphabet);
 printf("\n");
 return (0);
 }
