005fd071ce0fc2ef6aa459f50d5f679b9197d557
Vichazz <oyeniyivt@gmail.com>
2022-12-09 19:51:24 -0800

    ...

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ * Result: returns 0
+ */
+int main(void)
+{
+char alphabet;
+int nums;
+
+for (nums = 0; nums < 10; nums++)
+putchar(nums % 10 + '0');
+for (alphabet = 'a'; alphabet <= f; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
