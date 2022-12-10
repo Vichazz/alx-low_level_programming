0756d145e5b1d333ae5307e47c782cdda30ca2a9
Vichazz <oyeniyivt@gmail.com>
2022-12-10 05:33:00 -0800

    Update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int n;
-for (n = 0; n < 10)
+for (n = 0; n < 10;)
 {
 putchar(n);
 n++;
@@ -15,7 +15,7 @@ if (n != 9)
 putchar(',');
 putchar(' ');
 }
-putchar('\n')
+putchar('\n');
 
 
 }
