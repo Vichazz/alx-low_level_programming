ad576ed0dae820eed55285c91aa5bf1fc1d6341e
Vichazz <oyeniyivt@gmail.com>
2022-12-09 17:38:09 -0800

    ...

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int alphabet;
 
-;for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+; for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 for (alphabet = 'A'; alphabet  <= 'Z'; alphabet++)
 putchar(alphabet);
 printf("\n");
