c80b155ceae7dd211c9a2845fe95105c960f8762
Vichazz <oyeniyivt@gmail.com>
2022-12-09 18:00:55 -0800

    ...

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program start
+ * Return: returns 0
+ */
+
+int main(void)
+{
+int alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q' || aphabet == 'e')
+continue;
+putchar (alphabet);
+}
+printf("\n");
+return (0);
+
+}
