6c07f19353117dd6e7315fefc125c569ea215713
Vichazz <oyeniyivt@gmail.com>
2022-12-13 10:16:50 -0800

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_islower - program entry point
+ *Return: returns 0
+ */
+int _islower(int c)
+{
+for (c == 'A' && c <= 'Z')
+{
+if (c = 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
+}
