fc5d0317afec3de04cf871d3e6da7665ee9b3f98
Vichazz <oyeniyivt@gmail.com>
2022-12-13 07:53:34 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,10 @@
 void print_alphabet_x10(void)
 {
 char goat;
-int mul;
 char final;
 
 for (goat = 'a'; goat = 'z'; goat++)
-int mul = 10;
-char final = goat * mul;
+for (final = goat; final <= 10; final++)
 _putchar(final);
 _putchar('\n');
 }
