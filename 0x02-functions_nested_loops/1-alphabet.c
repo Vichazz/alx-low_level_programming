f876b9ab90e46c48eaa2da79f89a8431cb1505b0
Vichazz <oyeniyivt@gmail.com>
2022-12-13 06:04:05 -0800

    Update4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #inlucde "main.h"
 /**
- *print_alphabet_x10 - program start here
+ *print_alphabet_ - program start here
  *Return: returns 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet(void);
 {
 char goat;
 for (goat = 'a'; goat =< 'z'; goat++)
