7d62178629efb5db4f0b90c85b0cc16d05219a56
Vichazz <oyeniyivt@gmail.com>
2022-12-13 07:26:13 -0800

    Updatr

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** 
+ * print_alphabet_x10 - start program
+ * Return: returns 0
+ */
+
+void print_alphabet_x10(void)
+{
+char goat;
+int mul;
+char final;
+
+for (goat = 'a'; goat = 'z'; goat++)
+int mul = 10;
+char final = goat * mul;
+_putchar(final);
+_putchar('\n');
+}
