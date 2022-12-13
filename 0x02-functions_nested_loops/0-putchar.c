aa3c10d077e01c1c4097a9ee9784a901671da167
Vichazz <oyeniyivt@gmail.com>
2022-12-13 05:10:19 -0800

    Update1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - start code
+ * Return: returns 0
+ */
+
+int main(void)
+{
+char g = '_';
+char p = 'p';
+char q = 'u';
+char r = 't';
+char s = 'c';
+char t = 'h';
+char u = 'a';
+char v = 'r';
+  
+_putchar(g);
+_putchar(p);
+ _putchar(q);
+ _putchar(r);
+ _putchar(s);
+ _putchar(t);
+ _putchar(u);
+ _putchar(v);
+ _putchar('\n');
+ 
+return (0);
+
+}
