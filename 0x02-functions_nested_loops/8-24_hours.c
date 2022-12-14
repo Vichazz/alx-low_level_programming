761ad89cebfa7482bd6fd417e9285cfc16c0d32c
Vichazz <oyeniyivt@gmail.com>
2022-12-13 20:13:55 -0800

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - program execution
+ * Return: returns 0
+ */
+
+void jack_bauer(void)
+{
+int hour, min;
+for (hour = 0; hour <= 23; hour++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour/10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putcgar((min / 10) + '0');
+_putchar('\n');
+}
+
+}
+
+}
