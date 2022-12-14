3d6850ebac167073a1b00668d536411681e5a5a8
Vichazz <oyeniyivt@gmail.com>
2022-12-13 20:16:08 -0800

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ for (hour = 0; hour <= 23; hour++)
 for (min = 0; min <= 59; min++)
 {
 _putchar((hour / 10) + '0');
-_putchar((hour/10) + '0');
+_putchar((hour / 10) + '0');
 _putchar(':');
 _putchar((min / 10) + '0');
-_putcgar((min / 10) + '0');
+_putchar((min / 10) + '0');
 _putchar('\n');
 }
 
