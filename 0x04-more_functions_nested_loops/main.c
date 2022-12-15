c7a1bf01dbce28d88c916c91142fa794853bca30
Vichazz <oyeniyivt@gmail.com>
2022-12-15 08:42:26 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program start
+ * Return: returns 0
+ */
+
+int main(void)
+{
+char c = 'G';
+printf("%c is %d", c, _isupper(c));
+return (0);
+}
