b83a9ca70f2ffdeaa6dce9d659a34723ed27bc4f
Vichazz <oyeniyivt@gmail.com>
2023-01-09 13:32:50 -0800

    Update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: return 0
  */
 
-int main(int argc, char __attribute__((unused)) *argv)
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,4 +12,5 @@ int main(int argc, char *argv[])
 int i;
 for (i = 0; i < argc; i++)
 printf("%s\n", argv[i]);
+return (0);
 }
