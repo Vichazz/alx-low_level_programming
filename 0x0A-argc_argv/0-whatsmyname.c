a877e25b63c7eeffb1128f6d61f1a4d6193c8f2e
Vichazz <oyeniyivt@gmail.com>
2023-01-09 10:17:24 -0800

    Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: return 0 
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return(0);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program start
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: return 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv)
+{
+printf("%d\n", argc);
+return(0);
+}
