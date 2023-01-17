ced95224330cd358ec90659d6d534cc106665113
Vichazz <oyeniyivt@gmail.com>
2023-01-17 09:16:16 -0800

    Task

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _object_like_macro_h_
-#define _object_like_macro_h_
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* _object_like_macro_h_ */
+#endif
