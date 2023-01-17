355a27a8fa48349d09e4cb2bb2bf59170d66923c
Vichazz <oyeniyivt@gmail.com>
2023-01-17 09:36:48 -0800

    Task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,16 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
+if (d != NULL)
+{
 struct dog *d
 {
 char *name;
 float age;
 char *owner;
 };
-
+}
 d->name = name;
 d->age = age;
 d->owner = owner;
