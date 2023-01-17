65e9f1ebd7693a98ffe21f13c55da18fa4da748b
Vichazz <oyeniyivt@gmail.com>
2023-01-17 11:59:50 -0800

    push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,7 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
+
 if (d->name == NULL)
 printf("Name: (nil)\n");
 else
@@ -17,7 +18,7 @@ printf("Name: %s\n", d->name);
 if (d->age < 0)
 printf("Age: (nil)\n");
 else
-printf("Age: %d\n", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
