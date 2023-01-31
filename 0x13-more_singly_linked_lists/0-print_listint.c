842bfca006d0feac8589d7fa935fe77faf39d08a
Vichazz <oyeniyivt@gmail.com>
2023-01-31 13:15:29 -0800

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,19 +3,18 @@
 /**
  *  print_listint - program function
  * @h: function parameter
- * return: 0
+ * Return: return count
  */
 
 
 size_t print_listint(const listint_t *h)
 {
 int count = 0;
-  
-while(h != NULL)
+while (h != NULL)
 {
 printf("%d\n", h->n);
 count++;
 h =  h->next;
 }
-return(count);
+return (count);
 }
