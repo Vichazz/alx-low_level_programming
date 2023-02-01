7d0c9be7041c89bb23c2b8412bd8960a27c7de87
Vichazz <oyeniyivt@gmail.com>
2023-01-31 18:44:17 -0800

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,87 +1,37 @@
 #include "lists.h"
-#include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
- */
-size_t looped_listint_len(const listint_t *head)
-{
-const listint_t *tortoise, *hare;
-size_t nodes = 1;
-
-if (head == NULL || head->next == NULL)
-return (0);
-
-tortoise = head->next;
-hare = (head->next)->next;
-
-while (hare)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
-}
-
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-}
-
-return (nodes);
-}
-
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
-
-return (0);
-}
-
-/**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: The number of nodes in the list.
- */
+* print_listint_safe - function that prints a safe linked listint with a loop.
+* @head: data type double pointer the head/next node
+* Return: new_node
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nodes, index = 0;
+const listint_t *tmp_n = NULL;
+const listint_t *l_n = NULL;
+size_t counter = 0;
+size_t new_n;
 
-nodes = looped_listint_len(head);
-
-if (nodes == 0)
+tmp_n = head;
+while (tmp_n)
 {
-for (; head != NULL; nodes++)
+printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
+counter++;
+tmp_n = tmp_n->next;
+l_n = head;
+new_n = 0;
+while (new_n < counter)
 {
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-}
-
-else
+if (tmp_n == l_n)
 {
-for (index = 0; index < nodes; index++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+return (counter);
 }
-
-printf("-> [%p] %d\n", (void *)head, head->n);
+l_n = l_n->next;
+new_n++;
 }
-
-return (nodes);
+if (!head)
+exit(98);
+}
+return (counter);
 }
